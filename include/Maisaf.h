2a20dae5b58978ae99996221bb9017a0be425f3e
Mai-saf <maimouna.barry@univ-tlse3.fr>
2025-01-01 21:15:42 +0100

    Update Maisaf.h

--- include/Maisaf.h
+++ include/Maisaf.h
@@ -1,22 +1,25 @@
 #include <stdio.h>
+#include<stdlib.h>
+typedef struct position vitesse temp param
 struct position{
 double x;
 double y;
 double z;
-}
+};
 
 struct vitesse{
 double dx;
 double dy;
 double dz;
-}
+};
+
 struct temp{ 
 double dt;
 double t;
-}
+};
 
 struct param{
 double sigma;
 double rho;
 double beta;
-}
+};
