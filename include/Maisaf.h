85c4b5c1ffe5cd91698a7b36a88f3fbb93d8c05d
Mai-saf <maimouna.barry@univ-tlse3.fr>
2025-01-01 20:20:32 +0100

    Update Maisaf.h

--- include/Maisaf.h
+++ include/Maisaf.h
@@ -1,1 +1,22 @@
+#include <stdio.h>
+struct position{
+double x;
+double y;
+double z;
+}
 
+struct vitesse{
+double dx;
+double dy;
+double dz;
+}
+struct temp{ 
+double dt;
+double t;
+}
+
+struct param{
+double sigma;
+double rho;
+double beta;
+}
