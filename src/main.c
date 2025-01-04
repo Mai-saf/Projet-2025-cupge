6989b43b8c62e714bf15c80c9c7463d89e59765a
Safifi31 <safiyabekhti@gmail.com>
2025-01-04 20:46:38 +0000

    "j'ai créé la fonction main "

--- src/main.c
+++ src/main.c
@@ -1,16 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "systems.h"
+#include "utils.h"
+
+
 int main(){
     
-    double x,y,z;
-    double dt;
-    double rho, sigma, beta;
+    System sys;
+    double x, y, z;
+    double dt = 0.01;
     int Tmax;
-    printf ("Entrez l'increment");
-    scanf("%lf", &dt );
-    printf ("Entrez les positions initiales(x,y,z):");
-    scanf("%lf %lf %lf", &x, &y, &z);
-    printf ("Entrez le temps d'arrêt maximale");
-    scanf("%lf",&Tmax);
+    
+
+    if(!configure_system(&sys)){
+        printf("Erreur dans la configuration du système. Fin du programme.\n");
+        return 1;
+    }
+
+    get_initial_conditions(&x, &y, &z, &Tmax);
+
+    char *outplut_file = "trajectoire.txt";
+
+    initialize_file(outplut_file);
+
+    for (double t=0; t <= Tmax; t += dt){
+        save_to_file(outplut_file, t, x, y, z);
+        sys.update(&x, &y, &z, &dt, sys.params);
+
+    }
+    printf("Traectoire calculée et sauvegardée dnas '%s'\n", outplut_file);
+    FILE *gnuplot_pipe = popen("gnuplot -persistent", "w");
+    if (!gnuplot_pipe){
+        perror("Erreur au lancement de Gnuplot");
+        return 1;
+    
+    
+    }
+
+    fprintf(gnuplot_pipe, "set terminal qt size 600,600\n";
+    fprintf(gnuplot_pipe, "set âlette defined (0 'blue', 1 'green', 2 'yellow', 3 'red')\n");
+    fprintf(gnuplot_pipe, "set xlabel 'X'\n");
+    fprintf(gnuplot_pipe, "set ylabel 'Y\n");
+    fprintf(gnuplot_pipe, "set zlabel 'Z'\n");
+    fprintf(gnuplot_pipe, "set cblabel 'Temps'\n");
+
+    fprintf(gnuplot_pipe, "splot '%s' using 2:3:4:1 with linespoints palette title 'Trajet'\n", outplut_file);
+
+    fprintf(gnuplot_pipe, "pause -1\n");
+
+    pclose(gnuplot_pipe);
+
     return 0;
+
 }
 
 
